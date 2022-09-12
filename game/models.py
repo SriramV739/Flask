ee33753e93d74ef0cc8005c1be1b614d2ee363bd
unknown <krishayg>
2022-09-11 22:22:38 -0700

    a

--- game/models.py
+++ game/models.py
@@ -1,17 +1,17 @@
 from game import db
 #from flask_login import UserMixin
-class Game(db.Model):
+class Games(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
+    game=db.Column(db.Integer())
     questions=db.Column(db.String())
     code = db.Column(db.Integer(), nullable=False)
     time = db.Column(db.Integer())
     players=db.relationship('Players',backref='owned_game',lazy=True)
-#class Qcopies(db.Model):
-    #question_content=db.Column(db.String(length))
 class Players(db.Model):
     id=db.Column(db.Integer(),primary_key=True)
     name=db.Column(db.String(length=15))
     score=db.Column(db.Integer())
-    game=db.Column(db.Integer(),db.ForeignKey('game.id'))
+    game=db.Column(db.Integer(),db.ForeignKey('games.id'))
     streak=db.Column(db.Integer())
     result=db.Column(db.String())
+    submission=db.Column(db.String())
--- game/routes.py
+++ game/routes.py
@@ -19,18 +19,20 @@ from flask import render_template, redirect,url_for, flash, request
 #from game.models import
 from game.forms import JoinForm,StartGameForm
 from game import db
-from game.models import Game,Players
+from game.models import Games,Players
 from sqlalchemy import Column, Integer, Float, Date, String, VARCHAR, select, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask_login import login_user,logout_user, login_required, current_user
+import random
 try:
     engine = create_engine(
         'sqlite:///verifyit.db',echo=False)
 except:
     print("Can't create 'engine")
-db.create_all()
+#db.drop_all()
+#db.create_all()
 meta_data=MetaData()
 
 conn=engine.connect()
@@ -45,12 +47,20 @@ def player():
 def join_page():
     form=JoinForm()
     if form.submit.data and form.validate():
-        #db.session.add()
-        newlink="/waiting/"+str(form.code.data)
-        return redirect(newlink)
+        if Games.query.filter_by(code=form.code.data).count()==0:
+            flash("Invalid code",category="danger")
+        elif Players.query.filter_by(name=form.name.data).filter_by(game=Games.query.filter_by(code=form.code.data).first().id):
+            flash("Name already in use. Please choose another name.", category="danger")
+        else:
+            db.session.add(Players(name=form.name.data,score=0,game=Games.query.filter_by(code=form.code.data).first().id
+                               ,streak=0,submission="",result=""))
+            db.session.commit()
+            newlink="/waiting/"+str(Players.query.filter_by(name=form.name.data).first().id)+"/"+\
+                    str(Games.query.filter_by(id=Players.query.filter_by(name=form.name.data).first().game).first().id)
+            return redirect(newlink)
     return render_template("join.html",form=form)
-@app.route("/waiting/<name>/<id>")
-def waiting_page(name,id):
+@app.route("/waiting/<playerid>/<gameid>")
+def waiting_page(name,playerid,gameid):
     return render_template("waitscreenplayer.html")
 @app.route("/question/<qnum>")
 def question_page(qnum):
@@ -59,6 +69,31 @@ def question_page(qnum):
 def start_page():
     form=StartGameForm()
     if form.submit.data:
-        gamechoice=request.form.get("game_choice")
-        print(gamechoice)
+
+        if request.form.get("game_choice"):
+            gamechoice = int(request.form.get("game_choice"))
+            gamerow=engine.execute("Select * FROM game_category_rows").fetchall()[gamechoice]
+            qry="Select id FROM question_rows WHERE categories LIKE '%"+gamerow[6]+"%'"
+            pqs=list(engine.execute(qry).fetchall())
+            #print(pqs)
+            qs=random.sample(pqs,gamerow[7])
+            print(qs)
+            qstr=""
+            for element in qs:
+                qstr+=(str(element[0])+",")
+            print(qstr)
+            print(gamerow)
+            print(gamechoice)
+            code = random.randint(100000, 999999)
+            while Games.query.filter_by(code=str(code)).count() > 0:
+                code = random.randint(100000, 999999)
+            db.session.add(Games(game=gamechoice,code=code,time=0,questions=qstr))
+            db.session.commit()
+            nl="/waiting/host/"+str(Games.query.filter_by(code=code).first().id)
+            return redirect(nl)
+        else:
+            flash("Please choose a game.",category="danger")
     return render_template("start.html",engine=engine,form=form)
+@app.route("/waiting/host/<id>")
+def waiting_host_page(id):
+    return render_template("waitscreenhost.html",game=Games.query.filter_by(id=id).first())
