e63e56614c55679fd98bf8e5951ee799fe91ccc5
unknown <krishayg>
2022-09-17 22:34:50 -0700

    a

--- game/__init__.py
+++ game/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 from sqlalchemy import create_engine
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///verifyit.db'
 app.config['SECRET_KEY']=('59d7ded4b4d238a1b4ac23fa')
 db = SQLAlchemy(app)
 socketio=SocketIO(app)
-
+socketio.init_app(app, cors_allowed_origins="*")
 # login_manager=LoginManager(app)
 # login_manager.login_view = "login_page"
 # login_manager.login_message_category="info"
