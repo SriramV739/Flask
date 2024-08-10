8521fe5034ec66258dd8e2dece69414646ae93ef
SriramV739 <79124797+SriramV739@users.noreply.github.com>
2024-08-10 15:13:22 -0700

    Cleanup Code- Turo Changes
    Code Cleanup
    Switch to Turso
    Move the session creation to init.py

--- game/__init__.py
+++ game/__init__.py
@@ -3,39 +3,32 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
 from sqlalchemy import create_engine
 from flask_socketio import SocketIO,send
+from sqlalchemy.orm import sessionmaker
+
 #test change
-import logging
+import logging,os
     
-import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lwvverifyit1@db.lyphnmeqkudqpefwvmeq.supabase.co:5432/postgres' #'sqlite:///verifyit.db'
-app.config['SECRET_KEY']=('59d7ded4b4d238a1b4ac23fa')
-db = SQLAlchemy(app)
-from game import models
-#db.drop_all()
-#db.create_all()
-#db.drop_all()
-#db.create_all()
-with app.app_context():
-    try:
-        # Begin a transaction
-        db.session.begin()
+TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lwvverifyit1@db.lyphnmeqkudqpefwvmeq.supabase.co:5432/postgres' #'sqlite:///verifyit.db'
+# app.config['SECRET_KEY']=('59d7ded4b4d238a1b4ac23fa')
+app.config['SECRET_KEY']=(TURSO_AUTH_TOKEN)
 
-        # ... your code here ...
-
-        # Commit the transaction if everything is successful
-        db.session.commit()
-    except Exception as e:
-        # Roll back the transaction in case of an error
-        db.session.rollback()
-        raise e
 socketio=SocketIO(app,logger=False)
 socketio.init_app(app, cors_allowed_origins="*")
 logging.getLogger('socketio').setLevel(logging.ERROR)
 logging.getLogger('engineio').setLevel(logging.ERROR)
 logging.getLogger('geventwebsocket.handler').setLevel(logging.ERROR)
-# login_manager=LoginManager(app)
-# login_manager.login_view = "login_page"
-# login_manager.login_message_category="info"
-from game import routes
+TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
+TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
 
+dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
+try:
+    engine = create_engine(dbUrl, connect_args={'check_same_thread': False}, echo=True)
+except:
+    print("Can't create engine")
+
+Session = sessionmaker(bind=engine)
+dbSession = Session()
+
+from game import routes
