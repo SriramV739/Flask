1c34baf04354e9b2c7e79890904757a102afc240
SriramV739 <79124797+SriramV739@users.noreply.github.com>
2024-08-10 15:19:18 -0700

    Switch to Turso

--- game/forms.py
+++ game/forms.py
@@ -3,25 +3,42 @@ from wtforms import StringField, PasswordField, SubmitField, IntegerField, Float
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError,InputRequired
 from game.models import Games
 from flask import session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, delete, func
+import os
 class JoinForm(FlaskForm):
     name=StringField(label="Name",validators=[Length(min=1,max=15)])
     code=StringField(label="Code")
     submit=SubmitField(label='Join')
+    
 class StartGameForm(FlaskForm):
     def validate_code(self,code):
+        TURSO_DATABASE_URL = os.environ.get("TURSO_DATABASE_URL")
+        TURSO_AUTH_TOKEN = os.environ.get("TURSO_AUTH_TOKEN")
+        dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
         try:
-            print(session['handles'])
-            if code.data in session['handles']:
+            engine = create_engine(dbUrl, connect_args={'check_same_thread': False}, echo=True)
+        except:
+            print("Can't create engine")
+
+        Session = sessionmaker(bind=engine)
+        dbSession = Session()
+        try:
+            handles=session.get("handles",[])
+            if code.data in handles:
                 pass
             else:
-                if Games.query.filter_by(code=code.data).count() > 0:
-                    print(Games.query.filter_by(code=code.data).count())
+                countInDb=dbSession.query(Games).filter(Games.code==code.data).count()
+                if countInDb>0:
+                    print(countInDb)
                     raise ValidationError('Code already exists! Please try a different code')
         except:
-            if Games.query.filter_by(code=code.data).count() > 0:
-                    print(Games.query.filter_by(code=code.data).count())
-                    raise ValidationError('Code already exists! Please try a different code')
+            countInDb=dbSession.query(Games).filter(Games.code==code.data).count()
+            if countInDb>0:
+                print(countInDb)
+                raise ValidationError('Code already exists! Please try a different code')
     code=StringField(validators=[DataRequired(),Length(min=3,max=11)])
     submit = SubmitField(label='Start')
+    
 class SubmitAnswerForm(FlaskForm):
     submitb=SubmitField(label='Submit')
